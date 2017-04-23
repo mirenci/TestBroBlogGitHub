f2857759682352620c15b1e3d8751acaf7ca711a
mirenci <skitiq@abv.bg>
2017-04-23 19:56:23 +0300

    Added hover on articles with jQuery and moved at the bottom script/BlogJs

--- Blog/Scripts/blog.js
+++ Blog/Scripts/blog.js
@@ -1,1 +1,20 @@
-﻿
+﻿$(function () {
+    var article = $('.articleWrapperAnchor div');
+    //var articleContent = $('.articleWrapperAnchor div p');
+    //var author = $('.author');
+
+    article.hover(function () {
+        // on mousein, change the background color
+        $(this).css('background-color', '#b9be9c');
+        //$(this).children('.articleContent').css('color', 'white');
+        //author.css('color', 'white');
+        //console.log($(this).children('.articleContent'));
+    }, function () {
+        // on mouseout, reset the background color
+        $(this).css('background-color', 'white');
+        //$(this).children('p').css('color', '#3d3f34');
+        //author.css('color', '#b4bcc2');
+        });
+
+    
+});
